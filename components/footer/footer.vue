b831bec7565164c09428553dee9972bfeb8b7e9c
Sky Liu <liutianyi.me@gmail.com>
2019-05-28 18:09:18 +0800

    Beta 0.0.5

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer id="footer">
     <div class="copyright">
-      <span>©️版权所有: 北京化工大学&赛尔网络有限公司</span>
-      <span class="record">京ICP备16033603号-4</span>
+      <span class="record">©️版权所有: 北京化工大学&赛尔网络有限公司</span>
+      <span>京ICP备16033603号-4</span>
     </div>
   </footer>
 </template>
@@ -14,20 +14,15 @@ export default {}
 <style lang="scss" scoped>
 footer {
   position: relative;
-  overflow: hidden;
   width: 100vw;
-  height: 50px;
   background-color: #004881;
   .copyright {
-    box-sizing: border-box;
-    position: absolute;
-    width: 100%;
+    line-height: 50px;
     bottom: 0;
     color: #fff;
-    padding: 10px 30px;
-    .record {
-      float: right;
-      // margin-right: 20px;
+    span {
+      display: inline-block;
+      margin: 0 40px;
     }
   }
 }
